1fcd24edb77aad14e6111ca6e6b52f23febebb6f
Dominic Narag <60565226+Dmncnrg@users.noreply.github.com>
2025-07-28 21:24:17 +0800

    update config

--- vite.config.ts
+++ vite.config.ts
@@ -4,9 +4,9 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "/HoyoCharacterBuildingSystem/",
   plugins: [
     react(),
     tailwindcss()
-  ],
-  base: "/HoyoCharacterBuildingSystem"
+  ]
 })
